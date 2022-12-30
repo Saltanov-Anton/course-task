c17ac2d1cd876b9e036d7fe7d2b0b5b3f4ab1fb7
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-30 15:08:43 +0900

    task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String[] array = {"Book1", "Author1", "Book2", "Author2", "Book3", "Author3"};
 
+        String[] array = {"Book1", "Author1", "Book2", "Author2", "Book3", "Author3"};
         for (int i = 0; i < array.length; i++) {
             if (i % 2 == 0) {
                 System.out.println("\"" + array[i] + "\"" + " " + array[++i]);

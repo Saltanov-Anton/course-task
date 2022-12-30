7a58015389629d33f2a83bacfcc8eb979f26dc78
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-30 15:08:04 +0900

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        String[] array = {"Book1", "Author1", "Book2", "Author2", "Book3", "Author3"};
 
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println("\"" + array[i] + "\"" + " " + array[++i]);
+            }
+        }
     }
 }
